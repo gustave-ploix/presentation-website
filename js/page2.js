6a7aaed24869bd74331858ea1c1755407c808fe3
gustave ploix <vilgotploix@gmail.com>
2021-06-14 11:44:14 +0200

    ajout d'une petite fonctionnalité au scroll

--- js/page2.js
+++ js/page2.js
@@ -1,3 +1,13 @@
 const body = document.querySelector('body');
+const barre = document.getElementById('barre')
 
 window.onload = body.classList.add('visible');
+
+window.onscroll = function() {scrollFunc()}
+
+scrollFunc = () => {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    barre.style.height = scrolled + "%";
+}
